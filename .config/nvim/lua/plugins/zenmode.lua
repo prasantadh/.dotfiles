110bcc6b46074dca645ea4ec479ffba91296e2d7
prasant adhikari <pa1038@nyu.edu>
2023-07-28 15:21:53 -0400

    [nvim] add zenmode

---
+++ .config/nvim/lua/plugins/zenmode.lua
@@ -0,0 +1,12 @@
+return 
+{
+    {
+        'folke/zen-mode.nvim',
+        opts = {
+            window = {
+                width = 80,
+                height = 1,
+            }
+        }
+    }
+}
