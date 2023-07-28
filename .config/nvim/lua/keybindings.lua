be0563bc8bc6358f0b4a7e5ca7483ed3e6344821
prasant adhikari <pa1038@nyu.edu>
2023-07-28 18:08:56 -0400

    [nvim] ctrl-hjkl motion on terminal mode

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -5,7 +5,7 @@ vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
 vim.keymap.set('n', '<leader>e', '<cmd>CocCommand explorer<cr>')
 
 -- navigation
-vim.keymap.set('n', '<C-h>', '<Cmd>NvimTmuxNavigateLeft<cr>')
-vim.keymap.set('n', '<C-j>', '<Cmd>NvimTmuxNavigateDown<cr>')
-vim.keymap.set('n', '<C-k>', '<Cmd>NvimTmuxNavigateUp<cr>')
-vim.keymap.set('n', '<C-l>', '<Cmd>NvimTmuxNavigateRight<cr>')
+vim.keymap.set({'n', 't'}, '<C-h>', '<Cmd>NvimTmuxNavigateLeft<cr>')
+vim.keymap.set({'n', 't'}, '<C-j>', '<Cmd>NvimTmuxNavigateDown<cr>')
+vim.keymap.set({'n', 't'}, '<C-k>', '<Cmd>NvimTmuxNavigateUp<cr>')
+vim.keymap.set({'n', 't'}, '<C-l>', '<Cmd>NvimTmuxNavigateRight<cr>')
