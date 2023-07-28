bcd1f5b06ef9bf578d090cc170041b8ca64192a7
prasant adhikari <pa1038@nyu.edu>
2023-07-27 21:03:11 -0400

    [lazy] cute and clever config hook

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -5,5 +5,13 @@ return
         lazy = false,
         priority = 1000,
         opts = {},
+        -- https://www.youtube.com/watch?v=aqlxqpHs-aQ
+        -- was super helpful on finding out about the config function
+        -- this is the default option but leaving here for code sample
+        config = function()
+            require('tokyonight').setup({
+                style="storm"
+            })
+        end
     }
 }
