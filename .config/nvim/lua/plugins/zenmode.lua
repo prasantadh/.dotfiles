d405d74c7c802bf521ca35ad9d5ae983dfc264b7
prasant adhikari <pa1038@nyu.edu>
2023-07-28 16:10:26 -0400

    [nvim] zenmode C-j C-k bindings

--- .config/nvim/lua/plugins/zenmode.lua
+++ .config/nvim/lua/plugins/zenmode.lua
@@ -7,6 +7,19 @@ return
                 width = 80,
                 height = 1,
             }
-        }
+        },
+        config = function() 
+            local actions = require("telescope.actions")
+            require('telescope').setup({
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<C-k>"] = actions.move_selection_previous,
+                            ["<C-j>"] = actions.move_selection_next
+                        }
+                    }
+                }
+            })
+        end
     }
 }
