0cc7949c288d6b10a16f8c8bdfe8ed02a91331ab
prasant adhikari <pa1038@nyu.edu>
2023-07-27 21:01:13 -0400

    [nvim] override plugin keybindings with mine

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,4 @@
+-- plugin manager and it's setting
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -13,4 +14,9 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
+-- override plugin defaults with some of my settings
+require("basics")
+require("keybindings")
+
+-- set colorscheme
 vim.cmd[[colorscheme tokyonight]]
