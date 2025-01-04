f3a9c2e464478786a03b8fc3a6d317049c8f0261
Prasant Adhikari <pa1038@nyu.edu>
2025-01-03 22:29:47 -0500

    install gobuster and ghidra

--- .kali.sh
+++ .kali.sh
@@ -63,6 +63,6 @@ curl -L https://github.com/dundee/gdu/releases/latest/download/gdu_linux_amd64.t
 chmod +x gdu_linux_amd64
 sudo mv gdu_linux_amd64 /usr/bin/gdu
 
-sudo apt install -y ripgrep vim-gtk3 gdb seclists nodejs 
+sudo apt install -y ripgrep vim-gtk3 gdb seclists nodejs gobuster ghidra
 echo "TODO: change emulation in terminal from default to linux"
 echo "TODO: Prefix+I inside tmux to install plugins"
